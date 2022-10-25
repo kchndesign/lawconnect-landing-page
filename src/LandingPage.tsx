d5b2fa6d5bb4923ecc3668421ed57c253b9cd90f
unknown <kevin@kchn.com.au>
2022-10-25 17:24:15 +1100

    Laid out section classes and hero styling

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,5 +1,9 @@
 import ActionButton from "./components/ActionButton/ActionButton";
 import NavBar from "./components/NavBar/NavBar";
+import HeroMobile from "./assets/images/lc-hero-mobile.png";
+import HeroMobile2x from "./assets/images/lc-hero-mobile@2x.png";
+import HeroDesktop from "./assets/images/lc-hero-desktop.png";
+import HeroDesktop2x from "./assets/images/lc-hero-desktop@2x.png";
 import "./LandingPage.scss";
 
 function LandingPage(): JSX.Element {
@@ -20,20 +24,57 @@ function LandingPage(): JSX.Element {
             
             */}
 
-            <section className="section section--side-by-side">
-                <div className="section__text">
-                    <h1>The place to work with your lawyer</h1>
-                    <p>
-                        Your lawyer uses LawConnect to share information with
-                        you about your legal case in a private, convenient and
-                        secure way.
-                    </p>
-                    <ActionButton text="Create Free Account" />
-                </div>
-                <div className="section__image">
-                    <img src="" alt="" />
-                </div>
-            </section>
+            <div className="section__wrapper" id="heroSectionWrapper">
+                <section className="section section--side-by-side">
+                    {/* 
+                    
+                    Hero section text content
+                    
+                    */}
+                    <div className="section__text section__text--hero">
+                        <h1 id="heroTitle">
+                            The place to work with your lawyer
+                        </h1>
+                        <p id="heroText">
+                            Your lawyer uses LawConnect to share information
+                            with you about your legal case in a private,
+                            convenient and secure way.
+                        </p>
+                        <ActionButton
+                            text="Create Free Account"
+                            id="heroButton"
+                        />
+                    </div>
+
+                    {/* 
+                    
+                    Hero section images
+
+                    */}
+
+                    <div className="section__image">
+                        <picture>
+                            {/* mobile source */}
+                            <source
+                                media="(max-width: 864px)"
+                                sizes="(max-width: 400px) 285px, 570px"
+                                srcSet={`${HeroMobile}, ${HeroMobile2x}`}
+                            />
+                            {/* desktop source */}
+                            <source
+                                media="(min-width: 865px)"
+                                sizes="(max-width: 960px) 656px, 1312px"
+                                srcSet={`${HeroDesktop}, ${HeroDesktop2x}`}
+                            />
+                            {/* fallback image */}
+                            <img
+                                src={HeroMobile2x}
+                                alt="A screenshot of the application and two professionals"
+                            />
+                        </picture>
+                    </div>
+                </section>
+            </div>
         </>
     );
 }
--- src/components/ActionButton/ActionButton.tsx
+++ src/components/ActionButton/ActionButton.tsx
@@ -1,13 +1,19 @@
+import { idText } from "typescript";
 import Styles from "./ActionButton.module.scss";
 
 // declare props for component
 type ActionButtonProps = {
     text: string;
     action?: Function;
+    id?: string;
 };
 
 function ActionButton(props: ActionButtonProps) {
-    return <button className={Styles.actionButton}>{props.text}</button>;
+    return (
+        <button className={Styles.actionButton} id={props.id}>
+            {props.text}
+        </button>
+    );
 }
 
 export default ActionButton;
